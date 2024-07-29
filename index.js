37ff969eca9d3707db30a7982233807124e670f1
Macharia <machariaelvys@gmail.com>
2024-07-29 13:45:05 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+ 
+
+
+const names = [`Guadalupe`, `Ollie`, `Aki`];
+
+const writeCards = () => {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+};
+
+   countDown = () => {
+    let count = 0
+    while (count < 11 ){
+     console.log( count ++);  
+    }
+   }
+
